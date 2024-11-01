56462d1128218aa481656e22f40e217f7cb20334
Carlos <jose.carlosnascimento@hotmail.com>
2024-11-01 15:05:07 -0300

    Trabalhando com Dados

---
+++ RecuperandoDados.py
@@ -0,0 +1,12 @@
+import platform
+
+print("Distribuição do Sistema Operacional...........: ", platform.platform())
+print("Nome do Sistema Operacional ..................: ", platform.system())
+print("Versão do Sistema operacional ................: ", platform.release())
+print("Arquitetura...................................: ", platform.architecture())
+print("Nome do Computador............................: ", platform.node())
+print("tipo de Máquina...............................: ", platform.machine())
+print("Processoador..................................: ", platform.processor())
+print("versão do Python.........................: ", platform.python_version())
+
+
